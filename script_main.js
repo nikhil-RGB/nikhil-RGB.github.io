2d9bc1bb455e185319161d5f9cd2b833b200dcd5
nikhil-RGB <bleach.ichigo.anime@gmail.com>
2022-05-21 21:42:59 +0530

    Add code to remove image from page in case of mobile device

--- script_main.js
+++ script_main.js
@@ -1,18 +1,12 @@
 const view = (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ||
     (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform)));
-if (!view) {
-    //code here to add the app image previews to the articles
-    let para1 = document.getElementById("7402");
-    let image_cipher = document.createElement("img");
-    image_cipher.src = "images/cipherEG.png";
-    image_cipher.width = 600;
-    image_cipher.height = 450;
-
-    para1.appendChild(image_cipher);
-    let para2 = document.getElementById("7403");
-    let image_chainR = document.createElement("img");
-    image_chainR.src = "images/chainEG.png";
-    image_chainR.width = 600;
-    image_chainR.height = 450;
-    para2.appendChild(image_chainR);
+if (view) {
+    //code to remove image preview in case the user is on a mobile device
+    let min = 710;
+    let max = 711;
+    for (let i = 0; min <= max; ++min, ++i) {
+        let parent = document.getElementById(i + "");
+        let element = document.getElementById(min + "");
+        parent.removeChild(element);
+    }
 }
