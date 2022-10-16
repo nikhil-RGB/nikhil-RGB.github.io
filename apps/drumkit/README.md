cd490658f0f3e321edb7863e4ce059e4a8e63e0d
nikhil-RGB <bleach.ichigo.anime@gmail.com>
2022-10-16 17:26:59 +0530

    Update drumkit files

--- apps/drumkit/README.md
+++
@@ -1,2 +0,0 @@
-# DS-Community-Recruitment
-This repository contains a basic Drumkit, built for the DS Community recruitments.
--- apps/drumkit/script.js
+++ apps/drumkit/script.js
@@ -1,4 +1,19 @@
 const elems = ["SNARE", "RIDE-CYMBAL", "TOM", "FLOOR-TOM", "HIHAT-CYMBAL", "CRASH-CYMBAL", "CLAP", "DSTICKS", "SPARKS"];
+const key_codes = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'];
+
+document.addEventListener("keydown", (event) => {
+
+    const code = event.key;
+    let index = key_codes.indexOf(code);
+    if (index == -1) {
+        return;
+    }
+    let id = elems[index];
+    const butt = document.getElementById(id);
+    butt.click();
+});
+
+
 for (let i = 0; i < elems.length; i++) {
     {
         const element = document.getElementById(elems[i]);
