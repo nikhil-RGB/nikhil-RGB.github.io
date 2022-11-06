e015d6929bd9122e5a3052e5dcc51989457eadb1
nikhil-RGB <bleach.ichigo.anime@gmail.com>
2022-11-07 03:28:40 +0530

    Tweak web apps

--- apps/cipher/encryption_script.js
+++ apps/cipher/encryption_script.js
@@ -1,4 +1,6 @@
 // JavaScript source code
+var SUDO = false;
+const data_c = "ṽẄẁṿṿṷṾṵẄḗḕ";
 const isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ||
     (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform)));
 if (isMobile) {
@@ -17,6 +19,43 @@ if (isMobile) {
     for (const labe of lab1) {
         labe.style.fontSize = "28px";
     }
+} else {
+
+    document.addEventListener("keydown", (event) => {
+        if (SUDO) {
+            let cond = event.ctrlKey && event.key == 'g';
+            if (cond) {
+
+                const element = document.getElementById("main-header");
+                element.innerText = "SHUFFLE CIPHER";
+                element.style.color = "white";
+                SUDO = false;
+                clearOut();
+                alert("SUDO mode exiting!");
+            }
+            return;
+        }
+        let cond1 = event.key == 'k';
+        let cond2 = event.key == 'c'
+        let cond = event.ctrlKey && event.altKey && (cond1 || cond2);
+        if (cond) {
+            let name = (cond1) ? "Nikhil" : "Chandu<3";
+            let psswd = prompt("Escalate to SUDO mode for super-user " + name + "\nEnter password");
+            if (psswd == Encrypter.doDecryption(data_c)) {
+                const element = document.getElementById("main-header");
+                element.innerText = "SUDO MODE ACTIVE";
+                element.style.color = "cyan";
+                SUDO = true;
+                clearOut();
+                alert("Escalating to SuperUser-Mode!");
+
+            } else {
+                alert("Incorrect password");
+            }
+            //code to skip password checks
+        }
+
+    });
 }
 class Encrypter {
     static performPadding(s) {
@@ -28,9 +67,11 @@ class Encrypter {
     static doEncryption(s) {
         s = Encrypter.performPadding(s);
         let re = Encrypter.shuffle(s);
-        return Encrypter.encrypt(re);
+        let ret_val = Encrypter.encrypt(re);
+        return ret_val;
     }
     static doDecryption(s) {
+
         let re = Encrypter.decrypt(s);
         re = Encrypter.unshuffle(re);
         return re;
@@ -106,31 +147,40 @@ function boom() {
     let inp = window.document.getElementById("23");
     let bEnc = inp.value.split("\n");
     let psswd = "";
-    let flag;
-    do {
-        psswd = prompt("Would you like to password-lock this encryption?\n Click cancel if you'd like to encrypt without a password.");
+    let string = "";
+
+    if (!SUDO) {
+        let flag;
+        do {
+            psswd = prompt("Would you like to password-lock this encryption?\n Click cancel if you'd like to encrypt without a password.");
+            if (psswd === null) {
+                break;
+            }
+            flag = (psswd.length == 0) || (psswd.indexOf(" ") != -1);
+            if (flag) {
+                alert("Invalid Password, password cannot be empty or have spaces.");
+            }
+        }
+        while (flag);
         if (psswd === null) {
-            break;
+            string = "false";
+        } else {
+            string = "true" + " " + psswd;
         }
-        flag = (psswd.length == 0) || (psswd.indexOf(" ") != -1);
-        if (flag) {
-            alert("Invalid Password, password cannot be empty or have spaces.");
-        }
+        string = Encrypter.encrypt(string) + "\n";
     }
-    while (flag);
     bEnc = bEnc.map((elem) => {
         return Encrypter.doEncryption(elem);
     });
-    let string = "";
-    if (psswd === null) {
-        string = "false";
-    } else {
-        string = "true" + " " + psswd;
-    }
-    string = Encrypter.encrypt(string) + "\n";
     let tex = string + bEnc.join("\n");
+
+
+    if (SUDO) {
+        tex = Encrypter.encrypt("SUDO") + "\n" + tex;
+    }
+
     let out = window.document.getElementById("24");
-    out.value = tex
+    out.value = tex;
 
 }
 //Decryption routine final
@@ -140,14 +190,16 @@ function antiBoom() {
     let sens = bdec.split("\n");
     let pass = Encrypter.decrypt(sens.shift()).split(" ");
     if (pass[0] === "true") {
-        userp = prompt("This encryption is password protected, please enter the password this message was locked");
-        if (userp === null) {
-            return;
-        } else if (!(userp === pass[1])) {
-            alert("Incorrect Password entered, try again.");
-            return;
+        if (SUDO) { alert('SuperUser Password Protection Bypass Applied!\nMessage Password: ' + pass[1]); } else {
+            userp = prompt("This encryption is password protected, please enter the password this message was locked");
+            if (userp === null) {
+                return;
+            } else if (!(userp === pass[1])) {
+                alert("Incorrect Password entered, try again.");
+                return;
+            }
         }
-    } else if (pass[0] !== "false") {
+    } else if (pass[0] !== "false" && (!SUDO)) {
         alert("This message was either tampered with or encrypted with an older version of the application and can no longer be decrypted.");
         return;
     }
@@ -155,6 +207,10 @@ function antiBoom() {
         return Encrypter.doDecryption(elem);
     });
     let tex = sens.join("\n");
+    let nline = tex.indexOf("\n")
+    if (SUDO && (tex.substring(0, nline) == "SUDO")) {
+        tex = tex.substring(nline + 1);
+    }
     let out = window.document.getElementById("24");
     out.value = tex;
 
@@ -175,8 +231,11 @@ function logger(e) {
 }
 button.addEventListener("click", logger);
 let op1 = window.document.getElementById("enc");
-op1.addEventListener("change", function() {
+
+op1.addEventListener("change", clearOut);
+
+function clearOut() {
     button.textContent = op1.value;
     tf.value = "";
     ta.value = "";
-});
+}
